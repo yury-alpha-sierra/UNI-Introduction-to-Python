cf629a5270dd2fca10142d2a82f5c9d8fe8d253a
Yury Gurevich <yury@alphasierra.com.au>
2018-03-06 20:43:18 +1100

    auto commits

--- assignment2_1.py
+++ assignment2_1.py
@@ -1,6 +1,7 @@
 import csv
 
-
-
 with open('.//data//Countries and Zones.csv','r') as contryAndZone:
     contryAndZoneReader = csv.reader(contryAndZone)
+    next(contryAndZoneReader)
+    for eachLine in contryAndZoneReader:
+        print(eachLine[0:2])
