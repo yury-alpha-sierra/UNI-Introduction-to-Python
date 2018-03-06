a909e9095e8266f54d5f409f3a3850a941f6fc63
Yury Gurevich <yury@alphasierra.com.au>
2018-03-07 08:54:12 +1100

    Changed to autocommit

--- assignment2_1.py
+++ assignment2_1.py
@@ -1,7 +1,12 @@
 import csv
 
-with open('.//data//Countries and Zones.csv','r') as contryAndZone:
+# this is a base directory where all the data/csv files are kept
+dataBaseDirectory = './/data//'
+
+with open(dataBaseDirectory + 'Countries and Zones.csv','r') as contryAndZone:
     contryAndZoneReader = csv.reader(contryAndZone)
-    next(contryAndZoneReader)
+
+    next(contryAndZoneReader)   # get rid of the csv file first row that had column headers in it
+
     for eachLine in contryAndZoneReader:
-        print(eachLine[0:2])
+        print(eachLine[0:2])   # make sure we are reading first two columns. Import using csv reader imports many more columns for some reason????
