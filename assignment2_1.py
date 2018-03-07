75379cfab32b313aed96b1b20b54f5bf3d71dbb1
Yury Gurevich <yury@alphasierra.com.au>
2018-03-07 16:42:13 +1100

    just save

--- assignment2_1.py
+++ assignment2_1.py
@@ -3,10 +3,24 @@ import csv
 # this is a base directory where all the data/csv files are kept
 dataBaseDirectory = './/data//'
 
-with open(dataBaseDirectory + 'Countries and Zones.csv','r') as contryAndZone:
-    contryAndZoneReader = csv.reader(contryAndZone)
 
-    next(contryAndZoneReader)   # get rid of the csv file first row that had column headers in it
+def importDataFromFile(fileName,column1,column2,column3,column4):
+    returnedDictionary = {column1:'',column2:'',column3:'',column4:'',}
 
-    for eachLine in contryAndZoneReader:
-        print(eachLine[0:2])   # make sure we are reading first two columns. Import using csv reader imports many more columns for some reason????
+    with open(dataBaseDirectory + fileName, 'r') as inputFile:
+        inputFileReader = csv.DictReader(inputFile)
+
+        for eachLine in inputFileReader:
+                returnedDictionary[eachLine[column1]] = eachLine[column2]
+    return returnedDictionary
+
+# countryAndZoneData = {}
+# countryAndZoneData = importDataFromFile('Countries and Zones.csv','Destination country','Zones')
+# print(len(countryAndZoneData))
+# print(countryAndZoneData)
+
+data = {}
+data = importDataFromFile('Economy Letters Price Guide ($).csv','Weight','Zone 1','Zone 2,3 and 5','Zone 4, 6, 7 and 9')
+print(len(data))
+print(data)
+
---
+++ data/testOfVariableArguments.py
@@ -0,0 +1,28 @@
+import csv
+
+# this is a base directory where all the data/csv files are kept
+dataBaseDirectory = './/data//'
+
+
+def importDataFromFile(fileName,column1,column2):
+    returnedDictionary = {}
+
+    with open(dataBaseDirectory + fileName, 'r') as inputFile:
+        inputFileReader = csv.DictReader(inputFile)
+
+        for eachLine in inputFileReader:
+                returnedDictionary[eachLine[column1]] = eachLine[column2]
+    return returnedDictionary
+
+countryAndZoneData = {}
+countryAndZoneData = importDataFromFile('Countries and Zones.csv','Destination country','Zones')
+print(len(countryAndZoneData))
+print(countryAndZoneData)
+
+countryAndZoneData = {}
+countryAndZoneData = importDataFromFile('Countries and Zones.csv','Destination country','Zones')
+print(len(countryAndZoneData))
+print(countryAndZoneData)
+
+country = 'Iran - Islamic Republic Of'
+print(countryAndZoneData.get(country))
