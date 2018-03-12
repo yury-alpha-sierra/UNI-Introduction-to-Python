d4a80c88de32d1b3d68c4f12d189f10028502fb0
Yury Gurevich <yury@alphasierra.com.au>
2018-03-12 18:19:54 +1100

    got them working

--- test3.py
+++ test3.py
@@ -1,29 +1,32 @@
-#!C:/Users/yury_/Anaconda3/python.exe
-# -*- coding: utf-8 -*-
-
-import csv
-import sys
-
 # this is a base directory where all the data/csv files are kept
+import csv 
 dataBaseDirectory = './/data//'
 
 def importEconomyLetterPriceDataFromCsvFile(filename,*argv):
     returnedDictionary = {}
+    # field_names = 'weight','zone 1','zone 2','zone 3'
 
     with open(dataBaseDirectory + filename, 'r') as inputFile:
         inputFileReader = csv.DictReader(inputFile,fieldnames=None)
-
+        # for arguments in argv[1:]:
+        #     print(arguments)
         for eachLine in inputFileReader:
-            returnedDictionary.update(eachLine)
+            k = eachLine.items()
+            # returnedDictionary[k] = eachLine[1:]
 
     return returnedDictionary
-
+# import csv
+# reader = csv.reader(open('filename.csv', 'r'))
+# d = {}
+# for row in reader:
+#    k, v = row
+#    d[k] = v
 economyLetterPriceData = {}
 economyLetterPriceData = importEconomyLetterPriceDataFromCsvFile('Economy Letters Price Guide ($).csv' ,'Col1','Col2','Col3','Col4')
 
 print(economyLetterPriceData.values())
-print(economyLetterPriceData.keys())
-for d in  economyLetterPriceData.keys():
-    print(d)
-for d in  economyLetterPriceData.values():
-    print(d)
+# print(economyLetterPriceData.keys())
+# for d in  economyLetterPriceData.keys():
+#     print(d)
+# for d in  economyLetterPriceData.values():
+#     print(d)
--- testingInput.py
+++ testingInput.py
@@ -1,8 +1,26 @@
 import pandas as pd
-df=pd.read_csv('.//data//Economy Letters Price Guide ($).csv', delimiter=',',header=None)
-# print(df.shape[0:])
-# print(df.ndim)
+import re as re
+dataBaseDirectory = './/data//'
 
-print(df.columns[0:])
-print(df.index)
-# print(df.data[0:])
+def importPriceDataFromCsvFile(filename):
+    returnedDictionary = {}
+
+    returnedDictionary = pd.read_csv(dataBaseDirectory + filename, header=0, index_col=0, squeeze=True).to_dict()
+
+    return returnedDictionary
+
+def getZoneLabel(zone):
+    zonePattern = re.compile('['+ str(zone) + ']')
+    y = None
+    for i in economyLetterPriceData.keys():
+        match = re.search(zonePattern,i)
+        if match is not None:
+            y = i
+    return y
+
+economyLetterPriceData = {}
+economyLetterPriceData = importPriceDataFromCsvFile('Economy Letters Price Guide ($).csv')
+
+zone = 6
+
+print(economyLetterPriceData.get(getZoneLabel(zone)).get('Up to 50g'))
