79ce006c08fba61951184e3fb95a4f131d3a2f82
Yury Gurevich <yury@alphasierra.com.au>
2018-04-08 01:43:25 +1000

    introduced -info- panel. Separated pannels into different funtions

--- ui.py
+++ ui.py
@@ -13,30 +13,14 @@ class Ui(wx.Frame):  # pylint: disable=too-many-ancestors
         self.application = app
 
         wx.Frame.__init__(self, parent, id, name, style=wx.DEFAULT_FRAME_STYLE & ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX | wx.MAXIMIZE_BOX | wx.MINIMIZE_BOX), size=(1000, 600))
-
         self.Bind(wx.EVT_CLOSE, self.my_frame_handle_EVT_CLOSE)
 
         self.my_font = wx.Font(14, wx.FONTFAMILY_ROMAN, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False, u'sans-serif')
 
         self.my_service_panel = wx.Panel(self)
         self.my_admin_panel = wx.Panel(self)
+        self.my_info_panel = wx.Panel(self)
 
-        self.my_service_panel.Hide()
-        self.my_admin_panel.Hide()
-
-        self.my_admin_panel.SetBackgroundColour("RED")
-
-        self._init_service_pane()
-
-    def _init_service_pane(self):
-        """[summary]
-        """
-        self.my_service_panel_colour = wx.Colour(230, 240, 255, alpha=wx.ALPHA_OPAQUE)
-        self.my_service_panel.SetBackgroundColour(self.my_service_panel_colour)
-
-        self.my_status_bar = self.CreateStatusBar(4)  # pylint: disable=unused-variable
-        self.my_status_bar.SetStatusWidths([200, 300, 200, 100])
-        self.my_status_bar.SetStatusText('Sale number {}'.format(self.application.get_next_sales_number()))
         self.my_menu_bar = wx.MenuBar()
         self.my_menu = wx.Menu()
 
@@ -51,6 +35,35 @@ class Ui(wx.Frame):  # pylint: disable=too-many-ancestors
         self.my_menu_bar.Append(self.my_menu, "Function Selector")
         self.my_menu_bar.SetFont(self.my_font)
         self.SetMenuBar(self.my_menu_bar)
+
+        self.my_service_panel.Hide()
+        self.my_admin_panel.Hide()
+
+        self.my_panel_boxsizer = wx.BoxSizer(wx.VERTICAL)
+        self.my_panel_boxsizer.Add(self.my_service_panel, 1, wx.ALL | wx.EXPAND, border=3)
+        self.my_panel_boxsizer.Add(self.my_admin_panel, 1, wx.ALL | wx.EXPAND, border=3)
+        self.my_panel_boxsizer.Add(self.my_info_panel, 1, wx.ALL | wx.EXPAND, border=3)
+
+        self.__init_admin_panel()
+        self.__init_service_panel()
+        self.__init_info_panel()
+
+    def __init_info_panel(self):
+
+        self.my_info_boxsizer = wx.BoxSizer(wx.HORIZONTAL)
+
+    def __init_admin_panel(self):
+
+        self.my_admin_panel.SetBackgroundColour("RED")
+
+    def __init_service_panel(self):
+        """[summary]
+        """
+        self.my_service_panel_colour = wx.Colour(230, 240, 255, alpha=wx.ALPHA_OPAQUE)
+        self.my_service_panel.SetBackgroundColour(self.my_service_panel_colour)
+
+        self.my_status_bar = self.CreateStatusBar(4)  # pylint: disable=unused-variable
+        self.my_status_bar.SetStatusWidths([200, 300, 200, 100])
 
         self.my_weight_boxsizer = wx.BoxSizer(wx.HORIZONTAL)
         self.my_weight_label = wx.StaticText(self.my_service_panel, id=wx.ID_ANY, label="  Enter item weight:")
@@ -104,11 +117,11 @@ class Ui(wx.Frame):  # pylint: disable=too-many-ancestors
         self.my_busket_item_list.AppendColumn('quantity', width=80)
         self.my_busket_item_list.AppendColumn('cost', width=80)
         self.my_busket_item_list.AppendColumn('each', width=100)
-
         self.my_busket_boxsizer.Add(self.my_busket_item_list, 10, wx.ALIGN_CENTER_VERTICAL | wx.ALIGN_CENTER_HORIZONTAL | wx.EXPAND)
-        self.my_panel_boxsizer = wx.BoxSizer(wx.VERTICAL)
-        self.my_panel_boxsizer.Add(self.my_service_panel, 1, wx.ALL | wx.EXPAND, border=3)
-        self.my_panel_boxsizer.Add(self.my_admin_panel, 1, wx.ALL | wx.EXPAND, border=3)
+
+        # self.my_panel_boxsizer = wx.BoxSizer(wx.VERTICAL)
+        # self.my_panel_boxsizer.Add(self.my_service_panel, 1, wx.ALL | wx.EXPAND, border=3)
+        # self.my_panel_boxsizer.Add(self.my_admin_panel, 1, wx.ALL | wx.EXPAND, border=3)
 
         self.my_service_boxsizer = wx.BoxSizer(wx.VERTICAL)
         self.my_service_boxsizer.Add(self.my_weight_boxsizer, 1, wx.ALIGN_TOP | wx.ALL | wx.EXPAND, border=3)
@@ -168,7 +181,7 @@ class Ui(wx.Frame):  # pylint: disable=too-many-ancestors
         for each_line in self.application.invoice:
             self.my_busket_item_list.Append(self.__prettyfy_list(each_line))
 
-        self.application.invoice = sorted(self.application.invoice, key=self.getKey)
+        self.application.invoice = sorted(self.application.invoice, key=self.__getKey)
 
         total_cost = 0
         total_items = 0
@@ -176,10 +189,11 @@ class Ui(wx.Frame):  # pylint: disable=too-many-ancestors
         for row in range(self.my_busket_item_list.GetItemCount()):
             number_pattern = re.compile(r'\d.{1,5}')
             number_matches = re.findall(number_pattern, self.my_busket_item_list.GetItem(row, 6).GetText())
-### TODO: find a way of selectig colum number from text name of the column                                      # pylint: disable=W0511
+                ### TODO: find a way of selectig colum number from text name of the column                                      # pylint: disable=W0511
             total_items += int(self.my_busket_item_list.GetItem(row, 5).GetText())
             total_cost += float(''.join(number_matches))
 
+        self.my_status_bar.SetStatusText('Sale number: {}'.format(self.application.get_next_sales_number()))
         self.my_status_bar.SetStatusText('Total: ${0:.2f}'.format(total_cost), 3)
         self.my_status_bar.SetStatusText('Items: {0}'.format(total_items), 2)
 
@@ -232,13 +246,14 @@ class Ui(wx.Frame):  # pylint: disable=too-many-ancestors
                 multiplier = 1000
             else:
                 multiplier = 1
-            self.application.current_weight = float(
-                self.application.current_weight) * float(multiplier)
+            self.application.current_weight = float(self.application.current_weight) * float(multiplier)
 
             self.application.available_serice_price_options.clear()
             self.application.available_serice_price_options = self.application.get_available_serice_price_options()
             for each_item in self.application.available_serice_price_options:
                 self.my_item_list.Append(each_item)
+
+            self.my_country_choice.SetSelection(-1)
 
     def __blank_all_service_fields(self):
         """[summary]
@@ -261,6 +276,7 @@ class Ui(wx.Frame):  # pylint: disable=too-many-ancestors
         """[summary]
         """
         self.SetTitle("Postal Service")
+        self.my_info_panel.Hide()
         self.my_admin_panel.Hide()
         self.my_service_panel.Show()
 
@@ -270,8 +286,8 @@ class Ui(wx.Frame):  # pylint: disable=too-many-ancestors
         """[summary]
         """
         self.SetTitle("Admin Service")
-        self.my_admin_panel.Show()
-
+        self.my_info_panel.Hide()
         self.my_service_panel.Hide()
+        self.my_admin_panel.Show()
 
         self.Layout()
